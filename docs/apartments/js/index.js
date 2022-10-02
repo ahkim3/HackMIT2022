8083561e274eabd78c8cd4689f7094035fbf9cf5
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2022-10-01 20:22:06 -0400

    Re init

---
+++ docs/apartments/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    search = document.getElementById('search');
+
+    search.addEventListener("click", () => {
+        city = document.getElementById('location').value;
+        price = document.getElementById("price").value;
+        num_rooms = document.getElementById("num_rooms").value;
+        
+        localStorage.setItem("city", city);
+        localStorage.setItem("price", price);
+        localStorage.setItem("num_rooms", num_rooms);
+    })
+});
